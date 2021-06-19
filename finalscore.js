7b4293f0e892ce8de1e59d3e9441ea1b3f64f077
ConacBreslin <conacbreslin@sky.com>
2021-06-19 15:35:01 +0000

    Add finalscore comments

--- finalscore.js
+++ finalscore.js
@@ -5,31 +5,31 @@ let finalScore = localStorage.getItem("finalScore");
 let maxQuestions = localStorage.getItem("maxQuestions")
 
 let finalScorePercent = Math.round(finalScore / maxQuestions * 100);
-let phunnyFauxbias = ['"Nomophobia" - The fear not having a working mobile phone', 
-'"Aibohphobia"- The Fear of Palindromes', 
-'"Anachrophobia" - The fear of being unstuck in time', 
-'"Anatidaephobia" - The fear that somewhere, somehow, a duck is watching you', 
-'"Luposlipaphobia" – The fear of being pursued by timber wolves around a kitchen table while wearing socks on a newly waxed floor', 
-'"Arachibutyrophobia" – The fear of peanut butter sticking to the roof of your mouth', 'Linonophobia - The fear of String', 
-'"Keanuphobia" – The fear of the actor Keanu Reeves', 
-'"Kinemortophobia" – The fear of Zombies', 
-'"Zemmiphobia" - Fear of the Great Mole Rat',
-'"Pogonophobia" – The fear of Beards',
-'"Condylacousticophobia" - The fear of the sound of cracking knuckles',
-'"Gowiththeflowbia" - The fear of being relaxed',
-'"Arachnurinalphobia" - The fear of spiders in your toilet',
-'"Hippopotomonstrosesquippedaliophobia" - The fear of long words'
+let phunnyFauxbias = ['"Nomophobia" - The fear not having a working mobile phone.', 
+'"Aibohphobia"- The Fear of Palindromes.', 
+'"Anachrophobia" - The fear of being unstuck in time.', 
+'"Anatidaephobia" - The fear that somewhere, somehow, a duck is watching you.', 
+'"Luposlipaphobia" – The fear of being pursued by timber wolves around a kitchen table while wearing socks on a newly waxed floor.', 
+'"Arachibutyrophobia" – The fear of peanut butter sticking to the roof of your mouth', 'Linonophobia - The fear of String.', 
+'"Keanuphobia" – The fear of the actor Keanu Reeves.', 
+'"Kinemortophobia" – The fear of Zombies.', 
+'"Zemmiphobia" - Fear of the Great Mole Rat.',
+'"Pogonophobia" – The fear of Beards.',
+'"Condylacousticophobia" - The fear of the sound of cracking knuckles.',
+'"Gowiththeflowbia" - The fear of being relaxed.',
+'"Arachnurinalphobia" - The fear of spiders in your toilet.',
+'"Hippopotomonstrosesquippedaliophobia" - The fear of long words.'
 ];
 
 
 let phunnyFauxbiaIndex = Math.floor(Math.random() * phunnyFauxbias.length);
 let randomPhunnyFauxbia = phunnyFauxbias[phunnyFauxbiaIndex];
 
-finalScoreText.innerHTML  = (finalScorePercent >= 80) ? `<p><i class="far fa-grin-stars"></i>Congratulations! You scored ${finalScorePercent}%. You are frighteningly good!</p>`:
-(finalScorePercent >= 60 ) ? `<p><i class="far fa-smile-beam"></i>Well Done! You scored ${finalScorePercent}%. That is scarily good!</p>`:
-(finalScorePercent >= 40) ? `<p><i class="far fa-laugh"></i> You scored ${finalScorePercent}%</p>. I'm afraid to tell you you could do better.`:
-(finalScorePercent >= 20) ? `<p><i class="far fa-sad-tear"></i> Oh dear you scored ${finalScorePercent}%, You did horror-ifically?</p>`:
-`<p><i class="far fa-sad-cry"></i> Oh dear, you got a "ph- 'oh-oh' -bia" by scoring ${finalScorePercent}%</p>`;
+finalScoreText.innerHTML  = (finalScorePercent >= 80) ? `<p><i class="far fa-grin-stars"></i> Congratulations! You scored ${finalScorePercent}%. You are frighteningly good!</p>`:
+(finalScorePercent >= 60 ) ? `<p><i class="far fa-smile-beam"></i> Well Done! You scored ${finalScorePercent}%. That is scarily good!</p>`:
+(finalScorePercent >= 40) ? `<p><i class="far fa-laugh"></i> You scored ${finalScorePercent}%. I'm afraid to say you could do better.</p>`:
+(finalScorePercent >= 20) ? `<p><i class="far fa-sad-tear"></i> Oh dear you scored ${finalScorePercent}%, You did horror-ifically.</p>`:
+`<p><i class="far fa-sad-cry"></i> Oh dear, you got a "ph- 'oh oh' -bia" by scoring ${finalScorePercent}%.</p>`;
 
 phunnyFauxbiaText.innerHTML = `<p>Here is a random Phunny Fauxbia for you <br> ${randomPhunnyFauxbia} </p>`;
 
